579a7adea3a449c19445b611038fca954385ffc4
YuJeongHyun <0703olivia@naver.com>
2025-01-03 14:08:31 +0900

    Locale setting

---
+++ locale/src/main/java/locale/controller/LocaleController.java
@@ -0,0 +1,13 @@
+package locale.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LocaleController {
+
+	@RequestMapping({ "/" })
+	public String index() {
+		return "index";
+	}
+}
