c9caebec7400143f9c83ea174d7219a8c7c9b072
YuJeongHyun <0703olivia@naver.com>
2025-01-03 11:32:03 +0900

    file upload

--- fileupload/src/main/java/fileupload/controller/FileUploadController.java
+++ fileupload/src/main/java/fileupload/controller/FileUploadController.java
@@ -1,14 +1,20 @@
 package fileupload.controller;
 
-import java.io.IOException;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import fileupload.service.FileUploadService;
+
 @Controller
 public class FileUploadController {
+	private final FileUploadService fileUploadService;
+	
+	public FileUploadController(FileUploadService fileUploadService) {
+		this.fileUploadService = fileUploadService;
+	}
 
 	@RequestMapping({ "/", "/form" })
 	public String form() {
@@ -16,19 +22,13 @@ public class FileUploadController {
 	}
 
 	@RequestMapping("/upload")
-	public String upload(@RequestParam("email") String email, @RequestParam("file") MultipartFile file) {
-		 if (file == null || file.isEmpty()) {
-		        System.out.println("No file uploaded or file is empty.");
-		        return "form"; // 폼으로 다시 이동
-		    }
-		 
-		try {
-			System.out.println(file.getOriginalFilename());
-			System.out.println(file.getSize());
-			System.out.println(file.getBytes().length);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+	public String upload(
+			@RequestParam("email") String email,
+			@RequestParam("file") MultipartFile file,
+			Model model
+			) {
+		String url = fileUploadService.restore(file);
+		model.addAttribute("url", url);
 
 		return "result";
 	}
